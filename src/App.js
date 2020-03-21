e39ce100c8f673895b638b3434c2235b95abed3b
annin401 <annin401@gmail.com>
2020-03-21 17:58:56 +0900

    feat: ルーティングした

--- src/App.js
+++ src/App.js
@@ -1,7 +1,18 @@
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+
+import Start from "./pages/Start";
+import View from "./pages/View";
 
 function App() {
-  return <div className="App"></div>;
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={Start} />
+        <Route exact path="/view/" component={View} />
+      </Switch>
+    </BrowserRouter>
+  );
 }
 
 export default App;
