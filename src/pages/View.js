723f11c8e31beee596d96a5fafda6c1328fd20c7
annin401 <annin401@gmail.com>
2020-03-22 03:39:07 +0900

    feat: ボタンを押したらデバイスを増やせるようにした

--- src/pages/View.js
+++ src/pages/View.js
@@ -1,8 +1,43 @@
-import React from "react";
-import Preview from "../components/Preview.js";
+import React, { useState } from "react";
+import { makeStyles } from "@material-ui/core/styles";
+
+import shortid from 'shortid';
+
+import AddButton from "../components/AddButton";
+import Divice1 from '../components/Device1';
+
+const useStyles = makeStyles(theme => ({
+  addButton: {
+    position: "fixed",
+    bottom: "25px",
+    right: "25px"
+  }
+}));
 
 const View = () => {
-  return <Preview />;
+  const classes = useStyles();
+  const [deviceList, setDeviceList] = useState([]);
+  const addDevice = () => {
+    // 画面が1枚以上あったら表示しない
+    if (deviceList.length >= 1) return;
+
+    const newDevice = {
+      src: "", // url
+      device: "", // deviceの名前
+      isExistFrame: false, // スマホやタブレットの枠が存在するか
+      isSideways: true // デバイスが横向いてるか
+    };
+
+    // 更新
+    setDeviceList([...deviceList, newDevice]);
+  };
+  return (
+    /* メインのページはこの中を書き換えて */
+    <>
+      {deviceList.map(() => <Divice1 key={shortid.generate()}/>)}
+      <AddButton className={classes.addButton} onClick={addDevice}/>
+    </>
+  );
 };
 
 export default View;
